326103a8e450635df9ff35b9c5f8f6d10d056eff
zybex86 <zybex1986@gmail.com>
2021-03-20 17:58:34 +0100

    new Item model and migrations

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,9 @@
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
+
+from lists.models import Item
+from lists.views import home_page
 
 
 class HomePageTest(TestCase):
@@ -6,6 +11,12 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.contend.decode(), expected_html)
 
     def test_can_save_a_POST_request(self):
         response = self.client.post(
@@ -15,3 +26,22 @@ class HomePageTest(TestCase):
             'A new list item', response.content.decode()
         )
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'Absolutnie pierwszy element listy'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Drugi element'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item, second_saved_item = saved_items
+        self.assertEqual(first_saved_item.text, 'Absolutnie pierwszy element listy')
+        self.assertEqual(second_saved_item.text, 'Drugi element')
